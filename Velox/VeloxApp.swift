59ed18a0349972c88acb009539b49b8496c727f7
motoryang <motoyangxy@outlook.com>
2026-06-28 05:18:11 +0800

    Initial Commit

---
+++ Velox/VeloxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloxApp.swift
+//  Velox
+//
+//  Created by yangxy on 2026/6/28.
+//
+
+import SwiftUI
+
+@main
+struct VeloxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
